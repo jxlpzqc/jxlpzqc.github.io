bbd086a0457d44e4537565fcfe8f5bc795400203
程子丘 <jxlpzqc@live.com>
2024-05-17 20:57:30 +0800

    feat: font family.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -10,6 +12,21 @@ export default {
 					"green": "var(--color-term-green)",
 					"normal": "var(--color-term-normal)",
 				}
+			},
+			fontFamily: {
+				"mono": [
+					"'Cascadia Code'",
+					"'Fira Code'",
+					"'SF Mono'",
+					"'JetBrains Mono'",
+					"'Hack'",
+					"'Ubuntu Mono'",
+					"'Consolas'",
+					"'Microsoft YaHei'",
+					"'PingFang SC'",
+					"'微软雅黑'",
+					"monospace"
+				],
 			},
 		},
 	},
