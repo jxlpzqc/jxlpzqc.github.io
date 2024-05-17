503154bf196cb77395e282e00df453d9234bd3e8
程子丘 <jxlpzqc@live.com>
2024-05-17 20:35:58 +0800

    feat: content config and hero image.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,10 +4,10 @@ const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
-		title: z.string(),
-		description: z.string(),
+		title: z.string().default("<Untitled>"),
+		description: z.string().optional(),
 		// Transform string to Date object
-		pubDate: z.coerce.date(),
+		pubDate: z.coerce.date().default(new Date("1970-01-01")),
 		tags: z.array(z.string()).optional(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
