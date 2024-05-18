2e30e118d4f1b915d5a32108233ec4d25cd2b5f4
程子丘 <jxlpzqc@live.com>
2024-05-18 10:31:20 +0800

    feat: markdown header link and github emoji.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import rehypeAutoLinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import remarkEmoji from 'remark-emoji';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +14,14 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind(), react()],
   image: {
     service: passthroughImageService(),
+  },
+  markdown: {
+    remarkPlugins: [
+      [remarkEmoji, { accessible: true }]
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutoLinkHeadings, { behavior: 'append' }]
+    ],
   }
 });
